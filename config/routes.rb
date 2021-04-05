cd6be4f6b0f960dc528c3fdc1ee6f3a3fc18f6f1
Marta Kask <marta.m.kask@gmail.com>
2021-04-05 14:38:37 +0200

    showpage fixes - ingredients and layout amends

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
 
 
-  resources :doses, only:  [:destroy]
+  resources :doses, only: [:destroy]
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
